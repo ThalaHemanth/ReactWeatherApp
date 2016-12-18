282f8c2e4778f20ac18ecd1b421681f89c0db642
ThalaHemanth <viruhemanth@gmail.com>
2016-12-18 15:13:10 +0530

    styling

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -4,17 +4,16 @@ var NavBar = require('NavBar');
 var Main = React.createClass({
     render: function() {
         return (
-          <div>
-
-            <NavBar/>
-            <h2>Main Component</h2>
-            {this.props.children}
-
-        </div>
-
-        );
-    }
-
+            <div>
+                <NavBar/>
+                <div className="row">
+                    <div className="columns medium-6 large-4 small-centered">
+                      {this.props.children}
+                    </div>
+                  </div>
+                </div>
+              );
+    }      
 });
 
 module.exports = Main;
